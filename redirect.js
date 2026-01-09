716ad5a5331097b1ab62e0cd2a7a21e617bd3e8c
am1880 <129534602+am1880@users.noreply.github.com>
2026-01-09 21:10:21 +0000

    pull

--- redirect.js
+++ redirect.js
@@ -1,3 +1,20 @@
-if (window.location.pathname.startsWith("/shorts")) {
-  window.location.replace("https://www.youtube.com/");
-}
+(function () {
+  let lastUrl = location.href;
+
+  function redirectIfShorts() {
+    if (location.pathname.startsWith("/shorts")) {
+      window.location.replace("https://www.youtube.com/");
+    }
+  }
+
+  // Run once on load
+  redirectIfShorts();
+
+  // Watch for URL changes (YouTube SPA behavior)
+  new MutationObserver(() => {
+    if (location.href !== lastUrl) {
+      lastUrl = location.href;
+      redirectIfShorts();
+    }
+  }).observe(document, { subtree: true, childList: true });
+})();
